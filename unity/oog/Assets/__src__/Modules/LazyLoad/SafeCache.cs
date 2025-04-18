d48e279527cbfd720614adb53740716ebd4caa78
AysonAudio <contact@aysonaudio.dev>
2025-04-18 13:05:03 -0700

    feat: add class for caching async assets

---
+++ unity/oog/Assets/__src__/Modules/LazyLoad/SafeCache.cs
@@ -0,0 +1,53 @@
+#nullable enable
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+namespace Oog.Modules.LazyLoad {
+
+/// <summary>
+/// An asynchronously loaded asset.
+/// On first access, load data.
+/// On subsequent accesses, use cached data.
+/// </summary>
+
+public class SafeCache<T> where T : ScriptableObject {
+
+                                            ////////////////////////////////////
+                                            #region Fields and Properties
+                                            ////////////////////////////////////
+
+    public string AssetAddress { get; }
+    AsyncOperationHandle<T>? _asyncLoader;
+
+                                                                      #endregion
+                                            ////////////////////////////////////
+                                            #region Constructors and Destructors
+                                            ////////////////////////////////////
+
+    public SafeCache(string address) => AssetAddress = address;
+    ~SafeCache() => _asyncLoader?.Release();
+
+                                                                      #endregion
+                                            ////////////////////////////////////
+                                            #region Public Methods
+                                            ////////////////////////////////////
+
+    public bool TryGetAsset(out ScriptableObject? asset) {
+        asset = null;
+        _asyncLoader ??= Addressables.LoadAssetAsync<T>(AssetAddress);
+        if (!_asyncLoader.Value.IsValid()) return false;
+
+        // If called again after previous failure, try again
+        if (_asyncLoader.Value is { IsDone: true, Status: AsyncOperationStatus.Failed })
+            _asyncLoader = Addressables.LoadAssetAsync<T>(AssetAddress);
+
+        // Get results
+        _asyncLoader.Value.WaitForCompletion();
+        if (_asyncLoader.Value.Status == AsyncOperationStatus.Failed) return false;
+        asset = _asyncLoader.Value.Result;
+        return true;
+    }
+
+                                                                      #endregion
+}
+}
