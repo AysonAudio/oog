493f9bfd35d5d2fb3c110c84fc9265aeb68162b1
AysonAudio <contact@aysonaudio.dev>
2025-05-26 20:04:07 -0700

    refactor: comment HoverPainter

--- unity/oog/Assets/__src__/Modules/Interactable/HoverPainter.mono.cs
+++ unity/oog/Assets/__src__/Modules/Interactable/HoverPainter.mono.cs
@@ -11,43 +11,66 @@ namespace Oog.Modules.Interactable {
 [RequireComponent(typeof(Renderer))]
 [RequireComponent(typeof(HoverSensor))]
 public class HoverPainter : MonoBehaviour {
+
+                                            ////////////////////////////////////
+                                            #region Public Fields and Properties
+                                            ////////////////////////////////////
+
+    // Serialized Fields
     public List<Material> materialsHighlightOff;
     public List<Material> materialsHighlightOn;
+
+                                                                      #endregion
+                                            ////////////////////////////////////
+                                            #region Private Fields
+                                            ////////////////////////////////////
+
+    // Cached Unity Components
+    internal HoverSensor Sensor;
+    internal Renderer Renderer => Sensor.Renderer;
+
+    // Event Callbacks
     Action<InputAction.CallbackContext> _pointerEnterCallback;
     Action<InputAction.CallbackContext> _pointerExitCallback;
-    HoverSensor _hoverSensor;
+
+                                                                      #endregion
+                                            ////////////////////////////////////
+                                            #region MonoBehaviour Event Funcs
+                                            ////////////////////////////////////
 
     /// <summary>
     /// Serializes materials.
     /// Registers HoverSensor callbacks.
     /// </summary>
-    protected void OnEnable() {
-        _hoverSensor ??= GetComponent<HoverSensor>();
-        _hoverSensor.enabled = true;
+    void OnEnable() {
+        Sensor ??= GetComponent<HoverSensor>();
+        Sensor.enabled = true;
 
         // If highlightOff materials are undefined, default to current Renderer materials.
         if (materialsHighlightOff.Count == 0)
-            _hoverSensor.Renderer.GetSharedMaterials(materialsHighlightOff);
+            Renderer.GetSharedMaterials(materialsHighlightOff);
 
         // Prevent duplicate callbacks.
         if (_pointerEnterCallback == null) {
-            _pointerEnterCallback = _ => _hoverSensor.Renderer.SetSharedMaterials(materialsHighlightOn);
-            _hoverSensor.OnPointerEnter += _pointerEnterCallback;
+            _pointerEnterCallback = _ => Renderer.SetSharedMaterials(materialsHighlightOn);
+            Sensor.OnPointerEnter += _pointerEnterCallback;
         }
         if (_pointerExitCallback != null) return;
-        _pointerExitCallback = _ => _hoverSensor.Renderer.SetSharedMaterials(materialsHighlightOff);
-        _hoverSensor.OnPointerExit += _pointerExitCallback;
+        _pointerExitCallback = _ => Renderer.SetSharedMaterials(materialsHighlightOff);
+        Sensor.OnPointerExit += _pointerExitCallback;
     }
 
     /// <summary>
     /// Frees up memory.
     /// Unregisters HoverSensor callbacks.
     /// </summary>
-    protected void OnDisable() {
-        _hoverSensor.OnPointerEnter -= _pointerEnterCallback;
-        _hoverSensor.OnPointerExit -= _pointerExitCallback;
+    void OnDisable() {
+        Sensor.OnPointerEnter -= _pointerEnterCallback;
+        Sensor.OnPointerExit -= _pointerExitCallback;
         _pointerEnterCallback = null;
         _pointerExitCallback = null;
     }
+
+                                                                      #endregion
 }
 }
